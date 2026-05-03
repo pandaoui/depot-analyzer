18c68a7a10271fb0a5d0bb53f23122944642494a
Panadda Chnaoui <166217320+pandaoui@users.noreply.github.com>
2026-05-03 18:31:19 +0200

    Add SQLite database with SQLAlchemy

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,30 @@
-from fastapi import FastAPI
-from portfolio import Portfolio
+from fastapi import FastAPI, Depends
 from schemas import StockCreate
 from models import Stock
-from database import Base, engine
+from database import Base, SessionLocal, engine
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-portfolio = Portfolio()
 
 @app.get("/")
 def root():
     return {"message": "Depot Analyzer läuft"}
 
 @app.get("/stocks")
-def get_stocks():
-    return portfolio.stocks
+def get_stocks(db: Session = Depends(get_db)):
+    return db.query(Stock).all()
 
 @app.post("/stocks")
-def create_stock(stock_data: StockCreate):
+def create_stock(stock_data: StockCreate, db: Session = Depends(get_db)):
     stock = Stock(
         symbol=stock_data.symbol,
         name=stock_data.name,
@@ -27,5 +33,6 @@ def create_stock(stock_data: StockCreate):
         purchase_date=stock_data.purchase_date,
         current_price=stock_data.current_price
     )
-    portfolio.add(stock)
+    db.add(stock)
+    db.commit()
     return {"message": f"Aktie {stock.symbol} hinzugefügt"}
